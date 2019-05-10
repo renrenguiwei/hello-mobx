e54a00b98690cc06663312de3cdc87b903d6e9bd
hfpp2012 <hfpp2012@gmail.com>
2019-05-10 17:45:53 +0800

    12_store

---
+++ src/stores/index.js
@@ -0,0 +1,16 @@
+import BirdStore from './BirdStore';
+import TodoListStore from './TodoListStore';
+
+// class Store {
+//   constructor() {
+//     this.BirdStore = BirdStore;
+//     this.TodoListStore = new TodoListStore();
+//   }
+// }
+//
+// export default new Store();
+
+export default {
+  BirdStore,
+  TodoListStore
+}
